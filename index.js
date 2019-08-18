8f9b8622bce8fa9a6b55a07e8057fc5ef5692c5c
Terrence Eve <tijayeve@gmail.com>
2019-08-18 18:35:39 -0500

    Changed banner and added fonts, working on add button functionality

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    function renderMovieCards (movieArray) {
-
-        function makeMovieCardHTML (currentMovie) {
-            return `<div class="movies-container">
-                        <div class="movie">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
-                                <div class="card-body">
-                                <h5 class="card-title"><a href="http://www.imdb.com/title/${currentMovie.imdbID}">${currentMovie.Title}</a></h5>
-                                    <p class="card-text">${currentMovie.Year}</p>
-                                    <a href="#" class="btn btn-primary">Add</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`
-        }
-        
-        const moviesList = movieArray.map(makeMovieCardHTML)
-        const finalHTML = moviesList.join('')
-        console.log(finalHTML)
-        return `${finalHTML}`
-    
-    }
-
-    const results = document.getElementById("results")
-    results.innerHTML = renderMovieCards(movieData)
-
-} )
-
-
-// $(document).ready(function () {
+// document.addEventListener('DOMContentLoaded', function() {
     
 //     function renderMovieCards (movieArray) {
 
@@ -50,24 +18,54 @@ document.addEventListener('DOMContentLoaded', function() {
 //                     </div>`
 //         }
         
-//         let singleMovieHTML = movieArray.map(makeMovieCardHTML).join('')
-//         let moviesHTMLS = singleMovieHTML.join('')
-//         console.log(movieHTML)
-//         return `${moviesHTML}`
+//         const moviesList = movieArray.map(makeMovieCardHTML)
+//         const finalHTML = moviesList.join('')
+//         console.log(finalHTML)
+//         return `${finalHTML}`
     
 //     }
+
+//     const results = document.getElementById("results")
+//     results.innerHTML = renderMovieCards(movieData)
+
+// } )
+
+$(document).ready(init) // As soon as the document is ready, the init (initialize) function is ran.
+
+function init () {      //function init
+    function renderMovieCards (movieArray) {
+
+        function makeMovieCardHTML (currentMovie) {
+            return `<div class="movies-container">
+                        <div class="movie">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
+                                <div class="card-body">
+                                <h5 class="card-title"><a href="http://www.imdb.com/title/${currentMovie.imdbID}">${currentMovie.Title}</a></h5>
+                                    <p class="card-text">${currentMovie.Year}</p>
+                                    <a href="#" class="btn btn-primary onclick="saveToWatchlist(${currentMovie.imdbID})">Add</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+        }
+        
+        const moviesList = movieArray.map(makeMovieCardHTML)
+        const finalHTML = moviesList.join('')
+        console.log(finalHTML)
+        return `${finalHTML}`
     
-//     function movies () {
-//         let movieResults = $(".results").html(renderMovieCards(movieData))
-//     }
+    }
 
-// })
+    function saveToWatchList(movie) {
+        //This function needs to save a movie to a list 
+    }
 
+    const searchForm = document.getElementById("search-form")   //names the search-form location in the DOM
 
-
-
-
-
-
-// const moviePromise = $.getJSON("data.js")
-//         .then(function () {
+    searchForm.addEventListener("click", function (evt) {   //listens for a click on the search button
+        evt.preventDefault();   //this prevents the movies from going away once appearing                                 
+        const results = document.getElementById("results")
+        results.innerHTML = renderMovieCards(movieData)     //enters the HTML created above into the 
+    })
+}
