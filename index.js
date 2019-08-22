4351d23bbbab395152ca067d0916c20592db3a0d
Terrence Eve <tijayeve@gmail.com>
2019-08-22 18:42:54 -0500

    added watchlist html and js

--- index.js
+++ index.js
@@ -36,36 +36,69 @@ function init () {      //function init
     function renderMovieCards (movieArray) {
 
         function makeMovieCardHTML (currentMovie) {
-            return `<div class="movies-container">
-                        <div class="movie">
+                return `<div class="movie">
                             <div class="card" style="width: 18rem;">
                                 <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
                                 <div class="card-body">
-                                <h5 class="card-title"><a href="http://www.imdb.com/title/${currentMovie.imdbID}">${currentMovie.Title}</a></h5>
+                                <h5 class="card-title"><a href="http://www.imdb.com/title/${'currentMovie.imdbID'}">${currentMovie.Title}</a></h5>
                                     <p class="card-text">${currentMovie.Year}</p>
-                                    <a href="#" class="btn btn-primary onclick="saveToWatchlist(${currentMovie.imdbID})">Add</a>
+                                    <button  class="btn btn-primary" onclick=saveToWatchList("${currentMovie.imdbID}")>Add</button>
                                 </div>
                             </div>
-                        </div>
-                    </div>`
+                        </div>`
         }
         
         const moviesList = movieArray.map(makeMovieCardHTML)
         const finalHTML = moviesList.join('')
-        console.log(finalHTML)
         return `${finalHTML}`
     
     }
 
-    function saveToWatchList(movie) {
-        //This function needs to save a movie to a list 
-    }
+ 
 
     const searchForm = document.getElementById("search-form")   //names the search-form location in the DOM
 
     searchForm.addEventListener("click", function (evt) {   //listens for a click on the search button
         evt.preventDefault();   //this prevents the movies from going away once appearing                                 
         const results = document.getElementById("results")
-        results.innerHTML = renderMovieCards(movieData)     //enters the HTML created above into the 
+        results.innerHTML = renderMovieCards(movieData)     //enters the HTML created above into the
+        // addEvent()
     })
+}
+
+// function addEvent() { //We got onclick to work, but it is no longer considered best practices because it mixes html and javascript. No one likes that. 
+//     var container = document.getElementById("results");
+//     results.addEventListener("click", function(event) {
+//         var target = event.target;
+
+//         console.log(target);
+//     }) 
+// }
+
+function saveToWatchList(imdbID) {
+    //This function needs to save a movie to a list
+    var movie = movieData.find(function(currentMovie) { //uses the .find() method to find the current movie in movieData array.
+        if (currentMovie.imdbID == imdbID) {
+            return true;
+        } else {
+            return false;
+        } //this anonymous function passes the current movie inside of it and returns the imdbID number
+    })
+    //gets the JSON data from the local storage under watchlist but it comes in as a string
+    //var watchlistJSON = localStorage.getItem('watchlist')
+    //parses the JSON string into an object that we can use
+    //var watchlist=  JSON.parse(watchlistJSON);
+    //if the item 'watchlist' is non-existent it turns it into an array that we can fill with movies
+    // if (typeof watchlist === "null") {
+    //     watchlist = []
+    // }
+    var watchlist = []
+    //pushes the movie to the watchlist which is now an array
+    watchlist.push(movie)
+    //turns the watchlist array into a string so that it can be placed back in local storage
+    watchlistJSON = JSON.stringify(watchlist)
+    //sets our new array as the new storage item
+    localStorage.setItem('watchlist', watchlistJSON)
+    console.log('save to watchlist!', movie)
+    
 }
---
+++ watchlist.js
@@ -0,0 +1,8 @@
+$(document).ready(loadWatchlist)
+
+function loadWatchlist () {
+    const watchlist = localStorage.getItem('watchlist')
+
+    
+}
+
